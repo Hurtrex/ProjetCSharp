2cd929a0f2d3a056db75c3376438e738c4c39028
Malcom Beaudoin <zemalcom.beaudoin@gmail.com>
2025-10-22 12:18:23 -0400

    Added the folder for the classes Added Placeholder otherwise the folder wont sync

--- ProjetC#/Control/Placeholder.cs
+++ ProjetC#/Control/Placeholder.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjetC_
+namespace ProjetC_.Control
 {
-    internal class Program
+    internal class Placeholder
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
